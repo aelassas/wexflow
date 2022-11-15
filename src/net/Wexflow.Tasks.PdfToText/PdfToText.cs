e4a94ef70c130edd0aa44e7d48ea335407cbe17b
Jan Borup Coyle <jbc@g-it.dk>
2022-11-15 22:24:45 +0100

    Implementation of PdfToText task.

---
+++ src/net/Wexflow.Tasks.PdfToText/PdfToText.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Wexflow.Core;
+using System.Xml.Linq;
+using System.IO;
+using System.Threading;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using static System.Net.Mime.MediaTypeNames;
+using System.Text;
+using Path = System.IO.Path;
+
+namespace Wexflow.Tasks.PdfToText
+{
+    public class PdfToText : Task
+    {
+        public PdfToText(XElement xe, Workflow wf)
+            : base(xe, wf)
+        {
+        }
+
+        public override TaskStatus Run()
+        {
+            Info("Extract TEXT from PDF files ...");
+
+            bool success = true;
+            bool atLeastOneSucceed = false;
+
+            var files = SelectFiles();
+
+            if (files.Length > 0)
+            {
+                foreach (FileInf pdfFile in files)
+                {
+                    try
+                    {                        
+                        string textPath = Path.Combine(Workflow.WorkflowTempFolder,
+                            string.Format("{0}_{1:yyyy-MM-dd-HH-mm-ss-fff}.txt", Path.GetFileNameWithoutExtension(pdfFile.FileName), DateTime.Now));
+
+                        //Document doc = new Document();
+                        using (StreamWriter doc = new StreamWriter(textPath))
+                        {
+                            PdfReader pdfReader = new PdfReader(pdfFile.FileName);
+                            // Add the text file contents
+
+                            for (int page = 1; page <= pdfReader.NumberOfPages; page++)
+                            {
+                                ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+                                string currentText = PdfTextExtractor.GetTextFromPage(pdfReader, page, strategy);
+
+                                currentText = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(currentText)));
+                                doc.Write(currentText);
+                            }
+                            pdfReader.Close();
+                            Files.Add(new FileInf(textPath, Id));
+                            InfoFormat("Textfile {0} generated from the file {1}", textPath, pdfFile.Path);
+
+                        }
+                        if (!atLeastOneSucceed) atLeastOneSucceed = true;
+                    }
+                    catch (ThreadAbortException)
+                    {
+                        throw;
+                    }
+                    catch (Exception e)
+                    {
+                        ErrorFormat("An error occured while converting the file {0}", e, pdfFile.Path);
+                        success = false;
+                    }
+                }  
+            }
+
+            var status = Status.Success;
+
+            if (!success && atLeastOneSucceed)
+            {
+                status = Status.Warning;
+            }
+            else if (!success)
+            {
+                status = Status.Error;
+            }
+
+            Info("Task finished.");
+            return new TaskStatus(status, false);
+        }
+    }
+}
