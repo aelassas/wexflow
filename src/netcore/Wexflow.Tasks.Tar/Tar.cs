a335f8bf8ebf8c9917557cadcff244661b662689
aelassas <akram.elassas@gmail.com>
2023-06-05 20:31:01 +0100

    Code refactoring

--- src/netcore/Wexflow.Tasks.Tar/Tar.cs
+++ src/netcore/Wexflow.Tasks.Tar/Tar.cs
@@ -57,51 +57,50 @@ namespace Wexflow.Tasks.Tar
 
                 try
                 {
-                    using (var tar = new TarOutputStream(File.Create(tarPath), Encoding.UTF8))
+                    using var tar = new TarOutputStream(File.Create(tarPath), Encoding.UTF8);
+
+                    foreach (FileInf file in files)
                     {
-                        foreach (FileInf file in files)
+                        using (Stream inputStream = File.OpenRead(file.Path))
                         {
-                            using (Stream inputStream = File.OpenRead(file.Path))
+                            long fileSize = inputStream.Length;
+
+                            // Create a tar entry named as appropriate. You can set the name to anything,
+                            // but avoid names starting with drive or UNC.
+                            var entry = TarEntry.CreateTarEntry(file.RenameToOrName);
+
+                            // Must set size, otherwise TarOutputStream will fail when output exceeds.
+                            entry.Size = fileSize;
+
+                            // Add the entry to the tar stream, before writing the data.
+                            tar.PutNextEntry(entry);
+
+                            var localBuffer = new byte[32 * 1024];
+                            while (true)
                             {
-                                long fileSize = inputStream.Length;
-
-                                // Create a tar entry named as appropriate. You can set the name to anything,
-                                // but avoid names starting with drive or UNC.
-                                var entry = TarEntry.CreateTarEntry(file.RenameToOrName);
-
-                                // Must set size, otherwise TarOutputStream will fail when output exceeds.
-                                entry.Size = fileSize;
-
-                                // Add the entry to the tar stream, before writing the data.
-                                tar.PutNextEntry(entry);
-
-                                var localBuffer = new byte[32 * 1024];
-                                while (true)
+                                var numRead = inputStream.Read(localBuffer, 0, localBuffer.Length);
+                                if (numRead <= 0)
                                 {
-                                    var numRead = inputStream.Read(localBuffer, 0, localBuffer.Length);
-                                    if (numRead <= 0)
-                                    {
-                                        break;
-                                    }
-                                    tar.Write(localBuffer, 0, numRead);
+                                    break;
                                 }
+                                tar.Write(localBuffer, 0, numRead);
                             }
-
-                            tar.CloseEntry();
                         }
 
-                        // Finish/Close arent needed strictly as the using statement does this automatically
-
-                        // Finish is important to ensure trailing information for a Zip file is appended.  Without this
-                        // the created file would be invalid.
-                        tar.Finish();
-
-                        // Close is important to wrap things up and unlock the file.
-                        tar.Close();
-
-                        InfoFormat("Tar {0} created.", tarPath);
-                        Files.Add(new FileInf(tarPath, Id));
+                        tar.CloseEntry();
                     }
+
+                    // Finish/Close arent needed strictly as the using statement does this automatically
+
+                    // Finish is important to ensure trailing information for a Zip file is appended.  Without this
+                    // the created file would be invalid.
+                    tar.Finish();
+
+                    // Close is important to wrap things up and unlock the file.
+                    tar.Close();
+
+                    InfoFormat("Tar {0} created.", tarPath);
+                    Files.Add(new FileInf(tarPath, Id));
                 }
                 catch (ThreadAbortException)
                 {
