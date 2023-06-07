28ea06b5b18898ab4b9d43dcb8b658fb1e5ac1ba
aelassas <akram.elassas@gmail.com>
2023-06-07 14:39:23 +0100

    Update WexflowServiceClient.cs

--- src/net/Wexflow.Core.Service.Client/WexflowServiceClient.cs
+++ src/net/Wexflow.Core.Service.Client/WexflowServiceClient.cs
@@ -37,7 +37,7 @@ namespace Wexflow.Core.Service.Client
         private static string Base64Encode(string plainText)
         {
             var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
-            return System.Convert.ToBase64String(plainTextBytes);
+            return Convert.ToBase64String(plainTextBytes);
         }
 
         public WorkflowInfo[] Search(string keyword, string username, string password)
