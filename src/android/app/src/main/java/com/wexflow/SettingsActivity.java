8c58a687364dfd7135118e5ad831dd6d8d504172
aelassas <akram.elassas@gmail.com>
2023-06-08 06:31:02 +0100

    Update SettingsActivity.java

--- src/android/app/src/main/java/com/wexflow/SettingsActivity.java
+++ src/android/app/src/main/java/com/wexflow/SettingsActivity.java
@@ -1,42 +1,30 @@
 package com.wexflow;
 
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
-import android.preference.PreferenceFragment;
 
-import java.util.List;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends AppCompatActivity {
 
     public static final String KEY_PREF_WEXFLOW_URI = "prefWexflowUri";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Display the fragment as the main content.
-        getFragmentManager().beginTransaction()
+        getSupportFragmentManager()
+                .beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
     }
 
     /**
-     * Populate the activity with the top-level headers.
-     */
-    @Override
-    public void onBuildHeaders(List<Header> target) {
-    }
-
-    /**
      * This fragment shows the preferences for the first header.
      */
-    public static class SettingsFragment extends PreferenceFragment {
+    public static class SettingsFragment extends PreferenceFragmentCompat {
         @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-
-            // Load the preferences from an XML resource
-            addPreferencesFromResource(R.xml.settings);
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.settings, rootKey);
         }
     }
 
