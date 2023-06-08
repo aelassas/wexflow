048f7689a31d2248371fe862cea7ed5be99a8fc9
aelassas <akram.elassas@gmail.com>
2023-06-08 07:01:36 +0100

    Update User.java

--- src/android/app/src/main/java/com/wexflow/User.java
+++ src/android/app/src/main/java/com/wexflow/User.java
@@ -1,28 +1,38 @@
 package com.wexflow;
 
+import androidx.annotation.NonNull;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class User {
+    private final String username;
     private final String password;
     private final UserProfile userProfile;
 
-    public User(String username, String password, UserProfile userProfile){
+    public User(String username, String password, UserProfile userProfile) {
+        this.username = username;
         this.password = password;
         this.userProfile = userProfile;
     }
 
-    public String getPassword(){
+    public String getPassword() {
         return this.password;
     }
 
-    public UserProfile getUserProfile(){
+    public UserProfile getUserProfile() {
         return this.userProfile;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return this.username;
     }
 
     static User fromJSONObject(JSONObject jsonObject) throws JSONException {
         return new User(
-                  jsonObject.getString("Username")
+                jsonObject.getString("Username")
                 , jsonObject.getString("Password")
                 , UserProfile.fromInteger(jsonObject.getInt("UserProfile"))
         );
