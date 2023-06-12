9412a666e1176a5c5fa74bde3f948c13edcbac8c
aelassas <akram.elassas@gmail.com>
2023-06-12 11:01:03 +0100

    Update FilesLoaderEx.cs

--- src/net/Wexflow.Tasks.FilesLoaderEx/FilesLoaderEx.cs
+++ src/net/Wexflow.Tasks.FilesLoaderEx/FilesLoaderEx.cs
@@ -160,12 +160,4 @@ namespace Wexflow.Tasks.FilesLoaderEx
             }
         }
     }
-
-    public static class MiscExtensions
-    {
-        public static IEnumerable<T> TakeLast<T>(this IEnumerable<T> source, int N)
-        {
-            return source.Skip(Math.Max(0, source.Count() - N));
-        }
-    }
 }
---
+++ src/net/Wexflow.Tasks.FilesLoaderEx/MiscExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Wexflow.Tasks.FilesLoaderEx
+{
+    public static class MiscExtensions
+    {
+        public static IEnumerable<T> TakeLast<T>(this IEnumerable<T> source, int N)
+        {
+            return source.Skip(Math.Max(0, source.Count() - N));
+        }
+    }
+}
