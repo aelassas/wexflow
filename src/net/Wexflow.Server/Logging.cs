304c0adeec28567522914f2f2cf70d81b3aea51a
aelassas <akram.elassas@gmail.com>
2023-06-12 11:03:06 +0100

    Update Wexflow.Server

---
+++ src/net/Wexflow.Server/Logging.cs
@@ -0,0 +1,8 @@
+﻿namespace Wexflow.Server
+{
+    public class Logging
+    {
+        public bool IncludeScopes { get; set; }
+        public LogLevel LogLevel { get; set; }
+    }
+}
---
+++ src/net/Wexflow.Server/Smtp.cs
@@ -0,0 +1,10 @@
+﻿namespace Wexflow.Server
+{
+    public class Smtp
+    {
+        public string Server { get; set; }
+        public string User { get; set; }
+        public string Pass { get; set; }
+        public string Port { get; set; }
+    }
+}
