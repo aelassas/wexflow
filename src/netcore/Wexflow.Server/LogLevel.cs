091c2c728c083bdbcdc6f1fe9813c670de78a0d9
aelassas <akram.elassas@gmail.com>
2023-06-12 11:05:12 +0100

    Update Wexflow.Server

---
+++ src/netcore/Wexflow.Server/LogLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace Wexflow.Server
+{
+    public class LogLevel
+    {
+        public string Default { get; set; }
+        public string System { get; set; }
+        public string Microsoft { get; set; }
+    }
+}
