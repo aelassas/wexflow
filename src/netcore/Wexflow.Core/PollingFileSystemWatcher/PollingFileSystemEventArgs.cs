b7851156b5b19a9adde46ed8003da8071113b037
aelassas <akram.elassas@gmail.com>
2023-06-15 22:21:58 +0100

    Update PollingFileSystemEventArgs.cs

--- src/netcore/Wexflow.Core/PollingFileSystemWatcher/PollingFileSystemEventArgs.cs
+++ src/netcore/Wexflow.Core/PollingFileSystemWatcher/PollingFileSystemEventArgs.cs
@@ -3,23 +3,18 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-#pragma warning disable 1591
+
+#pragma warning disable CS1591 // Commentaire XML manquant pour le type ou le membre visible publiquement
 namespace Wexflow.Core.PollingFileSystemWatcher
 {
-#pragma warning disable CS1591 // Commentaire XML manquant pour le type ou le membre visible publiquement
     public class PollingFileSystemEventArgs : EventArgs
-#pragma warning restore CS1591 // Commentaire XML manquant pour le type ou le membre visible publiquement
     {
-#pragma warning disable CS1591 // Commentaire XML manquant pour le type ou le membre visible publiquement
         public PollingFileSystemEventArgs(FileChange[] changes)
-#pragma warning restore CS1591 // Commentaire XML manquant pour le type ou le membre visible publiquement
         {
             Changes = changes;
         }
 
-#pragma warning disable CS1591 // Commentaire XML manquant pour le type ou le membre visible publiquement
         public FileChange[] Changes { get; }
-#pragma warning restore CS1591 // Commentaire XML manquant pour le type ou le membre visible publiquement
     }
 }
-#pragma warning restore 1591
+#pragma warning restore CS1591 // Commentaire XML manquant pour le type ou le membre visible publiquement
