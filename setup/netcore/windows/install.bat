30d0e984e793f9bc2d596094b0cfa625fb789138
aelassas <akram.elassas@gmail.com>
2023-07-01 06:36:30 +0100

    Update install.bat

--- setup/netcore/windows/install.bat
+++ setup/netcore/windows/install.bat
@@ -2,13 +2,13 @@ set WexflowTesting=C:\WexflowTesting
 set Wexflow=C:\Wexflow-netcore
 
 ::xcopy WexflowTesting\* %WexflowTesting%\ /s /e
-xcopy WexflowTesting\* %WexflowTesting%\ /s /e /d
+xcopy WexflowTesting\* %WexflowTesting%\ /s /e /d /y
 ::del %Wexflow%\Database\Wexflow.db
 ::del %Wexflow%\Database\Wexflow-log.db
 ::xcopy Wexflow-netcore\* %Wexflow%\ /s /e
-xcopy Wexflow-netcore\* %Wexflow%\ /s /e /d
-xcopy Wexflow-netcore\Wexflow.xml %Wexflow%\ /s /e
-xcopy Wexflow-netcore\TasksNames.json %Wexflow%\ /s /e
-xcopy Wexflow-netcore\TasksSettings.json %Wexflow%\ /s /e
+xcopy Wexflow-netcore\* %Wexflow%\ /s /e /d /y
+xcopy Wexflow-netcore\Wexflow.xml %Wexflow%\ /s /e /y
+xcopy Wexflow-netcore\TasksNames.json %Wexflow%\ /s /e /y
+xcopy Wexflow-netcore\TasksSettings.json %Wexflow%\ /s /e /y
 
 pause
