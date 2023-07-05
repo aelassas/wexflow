4fe1de88110a21117459d27b3aa466b42af5c0f2
aelassas <akram.elassas@gmail.com>
2023-07-05 13:17:36 +0100

    Update WexflowEngine.cs

---
+++ src/net/Wexflow.Core/DbType.cs
@@ -0,0 +1,50 @@
+﻿// ReSharper disable InconsistentNaming
+namespace Wexflow.Core
+{
+    /// <summary>
+    /// Database type
+    /// </summary>
+    public enum DbType
+    {
+        /// <summary>
+        /// LiteDB
+        /// </summary>
+        LiteDB,
+        /// <summary>
+        /// MongoDB
+        /// </summary>
+        MongoDB,
+        /// <summary>
+        /// RavenDB
+        /// </summary>
+        RavenDB,
+        /// <summary>
+        /// PostgreSQL
+        /// </summary>
+        PostgreSQL,
+        /// <summary>
+        /// SQLServer
+        /// </summary>
+        SQLServer,
+        /// <summary>
+        /// MySQL
+        /// </summary>
+        MySQL,
+        /// <summary>
+        /// SQLite
+        /// </summary>
+        SQLite,
+        /// <summary>
+        /// Firebird
+        /// </summary>
+        Firebird,
+        /// <summary>
+        /// Oracle
+        /// </summary>
+        Oracle,
+        /// <summary>
+        /// MariaDB
+        /// </summary>
+        MariaDB
+    }
+}
