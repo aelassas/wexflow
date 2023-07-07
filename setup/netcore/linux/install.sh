85bf79f3dbb0d88909123c6abeda1d8363b53312
aelassas <akram.elassas@gmail.com>
2023-07-07 19:33:28 +0100

    Add install.sh

---
+++ setup/netcore/linux/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo "$0 is not running as root. Try using sudo."
+    exit
+fi
+
+WEXFLOW_SERVICE="/opt/wexflow/wexflow.service"
+WEXFLOW_SERVICE_SYSTEMD="/etc/systemd/system/wexflow.service"
+
+if [ -e  $WEXFLOW_SERVICE_SYSTEMD ]
+then
+    echo "Updating wexflow systemd service..."
+    sudo cp $WEXFLOW_SERVICE $WEXFLOW_SERVICE_SYSTEMD
+    sudo systemctl daemon-reload
+    sudo systemctl restart wexflow.service
+    sudo systemctl status wexflow.service --no-pager
+else
+    echo "Installing wexflow systemd service..."
+    sudo cp $WEXFLOW_SERVICE $WEXFLOW_SERVICE_SYSTEMD
+    sudo systemctl enable wexflow.service
+    sudo systemctl start wexflow.service
+    sudo systemctl status wexflow.service --no-pager
+fi
