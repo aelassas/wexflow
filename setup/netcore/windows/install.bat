fc4e033ceb90d8ab32b198a040edef6764356e2b
aelassas <akram.elassas@gmail.com>
2025-07-09 23:51:30 +0100

    fix(setup): copy all files during setup without skipping by date comparison

--- setup/netcore/windows/install.bat
+++ setup/netcore/windows/install.bat
@@ -3,11 +3,7 @@
 set WexflowTesting=C:\WexflowTesting
 set Wexflow=C:\Wexflow-netcore
 
-::xcopy WexflowTesting\* %WexflowTesting%\ /s /e
-xcopy WexflowTesting\* %WexflowTesting%\ /s /e /d /y
-::del %Wexflow%\Database\Wexflow.db
-::del %Wexflow%\Database\Wexflow-log.db
-::xcopy Wexflow-netcore\* %Wexflow%\ /s /e
+xcopy "WexflowTesting\*" "%WexflowTesting%\" /s /e /y
 
 rem === Backup original database files ===
 move "%Wexflow%\Database\Wexflow.sqlite" "%TEMP%\Wexflow.sqlite.bak"
@@ -15,15 +11,15 @@ move "%Wexflow%\Database\Wexflow.db" "%TEMP%\Wexflow.db.bak"
 move "%Wexflow%\Database\Wexflow-log.db" "%TEMP%\Wexflow-log.db.bak"
 
 rem === Copy everything from source to Wexflow directory ===
-xcopy Wexflow-netcore\* %Wexflow%\ /s /e /d /y
+xcopy "Wexflow-netcore\*" "%Wexflow%\"/s /e /y
 
 rem === Restore original database files ===
 move "%TEMP%\Wexflow.sqlite.bak" "%Wexflow%\Database\Wexflow.sqlite"
 move "%TEMP%\Wexflow.db.bak" "%Wexflow%\Database\Wexflow.db"
 move "%TEMP%\Wexflow-log.db.bak" "%Wexflow%\Database\Wexflow-log.db"
 
-xcopy Wexflow-netcore\Wexflow.xml %Wexflow%\ /s /e /y
-xcopy Wexflow-netcore\TasksNames.json %Wexflow%\ /s /e /y
-xcopy Wexflow-netcore\TasksSettings.json %Wexflow%\ /s /e /y
+xcopy "Wexflow-netcore\Wexflow.xml" "%Wexflow%\"/s /e /y
+xcopy "Wexflow-netcore\TasksNames.json" "%Wexflow%\"/s /e /y
+xcopy "Wexflow-netcore\TasksSettings.json" "%Wexflow%\"/s /e /y
 
 pause
