7ab7d06ec97d58fe23ca0eec83a1227e5f43dab3
aelassas <akram.elassas@gmail.com>
2025-07-09 23:31:07 +0100

    fix(setup): keep original SQLite and LiteDB databases during setup

--- setup/netcore/windows/install.bat
+++ setup/netcore/windows/install.bat
@@ -1,3 +1,5 @@
+@echo off
+
 set WexflowTesting=C:\WexflowTesting
 set Wexflow=C:\Wexflow-netcore
 
@@ -6,7 +8,20 @@ xcopy WexflowTesting\* %WexflowTesting%\ /s /e /d /y
 ::del %Wexflow%\Database\Wexflow.db
 ::del %Wexflow%\Database\Wexflow-log.db
 ::xcopy Wexflow-netcore\* %Wexflow%\ /s /e
+
+rem === Backup original database files ===
+move "%Wexflow%\Database\Wexflow.sqlite" "%TEMP%\Wexflow.sqlite.bak"
+move "%Wexflow%\Database\Wexflow.db" "%TEMP%\Wexflow.db.bak"
+move "%Wexflow%\Database\Wexflow-log.db" "%TEMP%\Wexflow-log.db.bak"
+
+rem === Copy everything from source to Wexflow directory ===
 xcopy Wexflow-netcore\* %Wexflow%\ /s /e /d /y
+
+rem === Restore original database files ===
+move "%TEMP%\Wexflow.sqlite.bak" "%Wexflow%\Database\Wexflow.sqlite"
+move "%TEMP%\Wexflow.db.bak" "%Wexflow%\Database\Wexflow.db"
+move "%TEMP%\Wexflow-log.db.bak" "%Wexflow%\Database\Wexflow-log.db"
+
 xcopy Wexflow-netcore\Wexflow.xml %Wexflow%\ /s /e /y
 xcopy Wexflow-netcore\TasksNames.json %Wexflow%\ /s /e /y
 xcopy Wexflow-netcore\TasksSettings.json %Wexflow%\ /s /e /y
