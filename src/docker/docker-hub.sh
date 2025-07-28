bee48560678b352152bdbf4aa90720e88b0ea172
aelassas <akram.elassas@gmail.com>
2025-07-28 20:23:59 +0100

    feat(docker): update docker-compose.docker-hub.yml

--- src/docker/docker-hub.sh
+++ src/docker/docker-hub.sh
@@ -9,7 +9,7 @@ docker run -p 8000:8000 aelassas/wexflow:latest
 docker stop wexflow
 docker rm wexflow
 
-#Test locally
+# Test locally
 docker build -t wexflow-test .
 docker run -d -p 8000:8000 --name wexflow-test wexflow-test
 docker logs -f wexflow-test
