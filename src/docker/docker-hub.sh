31db6c8b6abc65d9f07e9dd384a071299ae52385
aelassas <akram.elassas@gmail.com>
2025-09-11 09:46:08 +0100

    feat(ci): add docker-hub.yml

--- src/docker/docker-hub.sh
+++ src/docker/docker-hub.sh
@@ -1,6 +1,13 @@
 # Copy local download of wexflow-linux-netcore.zip
 # which was pre-downloaded and unzipped from https://github.com/aelassas/wexflow/releases/latest
 
+# tag_name v9.9
+# download https://github.com/aelassas/wexflow/releases/download/v9.9/wexflow-9.9-linux-netcore.zip
+# unzip wexflow-9.9-linux-netcore.zip
+docker build -t aelassas/wexflow:v9.9 -t aelassas/wexflow:latest wexflow
+docker push aelassas/wexflow:v9.9
+docker push aelassas/wexflow:latest
+
 docker build -t aelassas/wexflow:v9.9 -t aelassas/wexflow:latest .
 docker run -d -p 8000:8000 --name wexflow aelassas/wexflow:latest
 docker push aelassas/wexflow:v9.9
